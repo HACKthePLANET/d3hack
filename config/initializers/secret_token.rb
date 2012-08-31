f6af0794662c7044651bdbb33a6b606335d65d99
Doug May <intuedge@gmail.com>
2012-08-31 12:50:47 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+D3hack::Application.config.secret_token = '6f3a92c697cc12e282ae8918744a26a17a8a4949761f0575ace8c0090ad5e8b068cdcb4c72fa92dc4374e00a7492e0bd6d2301efad426b5d84317e15dc7e11fb'
